0cc05f229adc50de5d3665613d8aa72785ae6151
std-fs <155996914+std-fs@users.noreply.github.com>
2024-02-03 18:37:26 +0800

    Integrated LIB + indev ADF

---
+++ src/build/client/script-repo/adf.lua
@@ -0,0 +1,11 @@
+local frame = loadstring(game:HttpGet("https://raw.githubusercontent.com/std-fs/ex.rlua/src/build/client/lib/uiv.lua", true))
+local fxl = frame:frame(" RE:DEFINED - 127.0.0.1; std_fs")
+local fxi = fxl:cat(" Exploit ")
+
+fxi:btn("Cash", function()
+    game:GetService("ReplicatedStorage")["Msg"]["DrawWeapon"]:InvokeServer(7000003,-math.huge)
+end)
+
+fxi:btn("Gems", function()
+    game:GetService("ReplicatedStorage")["Msg"]["DrawFruit"]:InvokeServer(-math.huge)
+end)
